10744e4a638121172d471d63f4c177a64b3ec647
Jacob Logas <jakeman893@live.com>
2015-10-21 22:51:17 -0400

    Init updates

--- Facial-MoCap/Facial-MoCap/main.cpp
+++ Facial-MoCap/Facial-MoCap/main.cpp
@@ -1,13 +1,46 @@
 #include "opencv\highgui.h"
-int main()
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-	cvNamedWindow("P2", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture("path to image");
-	IplImage* frame;
+	VideoCapture cap(0); // open the video camera no. 0
+
+	if (!cap.isOpened())  // if not success, exit program
+	{
+		cout << "Cannot open the video cam" << endl;
+		return -1;
+	}
+
+	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+	std::cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+	namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+
 	while (1)
 	{
-		frame = cvQueryFrame(capture);
-		if (!frame) break; cvShowImage("P2", frame); char c = cvWaitKey(0); if (c == 27) break;
+		Mat frame;
+
+		bool bSuccess = cap.read(frame); // read a new frame from video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		imshow("MyVideo", frame); //show the frame in "MyVideo" window
+
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
 	}
-	cvReleaseCapture(&capture); cvDestroyWindow("P2");
+	return 0;
+
 }
